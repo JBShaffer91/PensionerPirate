32ff1df63875e35d6b211e2ed5b35dc1d6bfb000
Justin Shaffer <justin@pensionerpirate.com>
2024-06-06 12:00:22 -0700

    Upload background image, add it to HeroSection

--- libertalia/src/App.js
+++ libertalia/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HeroSection />
       {/* You can add other components below */}
-      <header className="App-header">
-        {/* Placeholder for other sections, e.g., HeroSection */}
-        <p>Welcome to Pensioner Pirate</p>
-      </header>
     </div>
   );
 }
